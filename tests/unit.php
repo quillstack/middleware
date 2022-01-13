6c3f3c3684d96f5761ea1931bf9c221165416a9b
Radek Ziemniewicz <radek@quillstack.org>
2022-01-13 21:03:43 +0000

    Created tests for MiddlewareProvider

--- tests/unit.php
+++ tests/unit.php
@@ -2,11 +2,15 @@
 
 declare(strict_types=1);
 
+use Psr\Http\Server\RequestHandlerInterface;
+use Quillstack\Middleware\Tests\Mocks\MockController;
+
 $container = container();
 $container->addToConfig([
-    \Psr\Http\Server\RequestHandlerInterface::class => \Quillstack\Middleware\Tests\Mocks\MockController::class,
+    RequestHandlerInterface::class => MockController::class,
 ]);
 
 return [
+    \Quillstack\Middleware\Tests\Unit\TestFallbackHandlerMiddlewareProvider::class,
     \Quillstack\Middleware\Tests\Unit\TestMiddlewareProvider::class,
 ];
